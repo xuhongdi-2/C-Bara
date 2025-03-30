47ed86ede4b393f5c5e74d2333243abec6e201a1
xuhongdi <a1574478807@126.com>
2025-03-30 19:41:20 +0800

    紧急发版

--- 1_demo.c
+++ 1_demo.c
@@ -3,6 +3,6 @@ int main() {
     printf("Hello, World!\n");
     int thePoor_money = 62;
     int theRich_money = 620000;
-    int A_B = theRich_money-thePoor_money
+    int Difference = theRich_money-thePoor_money;
     return 0;
 }
