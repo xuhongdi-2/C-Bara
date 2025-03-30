967abd24fe94a5d257c52ff2b59cb0cdf6b36105
xuhongdi <a1574478807@126.com>
2025-03-30 19:09:17 +0800

    微型发版1

---
+++ 1_demo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, World!\n");
+    int thePoor_money = 62;
+    int theRich_money = 620000;
+    int A_B = theRich_money-thePoor_money
+    return 0;
+}
