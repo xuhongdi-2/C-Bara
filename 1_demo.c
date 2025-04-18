01f8fa8dce38b610a9554af04fdfdfb6d0b42340
xuhongdi <15724478807@139.com>
2025-04-18 21:49:44 +0800

    v.1.0.2

--- 1_demo.c
+++ 1_demo.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+
 int main() {
     printf("Hello, World!\n");
     int thePoor_money = 62;
     int theRich_money = 620000;
     int Difference = theRich_money-thePoor_money;
+    printf("v.1.0.2");
+    printf(" 2025 XuHongdi.");
     return 0;
 }
