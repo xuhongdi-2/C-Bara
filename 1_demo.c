87931b54489c96f38d8db5592a9ccfef6a668bfe
xuhongdi-2 <15724478807@139.com>
2025-05-19 17:40:52 +0800

    v1.1

--- 1_demo.c
+++ 1_demo.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     printf("\033[34mHappy Labour Day!");
     printf("\n");
     printf("\033[35mv.1.0.3\n");
     printf("\033[34m2025 XuHongdi.\n");
+    system("neofetch");
+    system("adb shell input keyevent 26"); //熄屏
+    system("adb shell input keyevent 26"); //亮屏
+    system("adb shell input keyevent 26"); //熄屏
+    system("adb shell input keyevent 26"); //亮屏   
+    system("adb shell input keyevent 26"); //熄屏
+    system("adb shell input keyevent 26"); //亮屏
+    system("adb shell input keyevent 26"); //熄屏
+    system("adb shell input keyevent 26"); //亮屏
     return 0;
+    
 }
