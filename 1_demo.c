08b36fb4149358c4344256142626e2e3f63ad06a
xuhongdi <15724478807@139.com>
2025-05-25 09:51:33 +0800

    v1.1

--- 1_demo.c
+++ 1_demo.c
@@ -4,7 +4,7 @@
 int main() {
     printf("\033[34mHappy Labour Day!");
     printf("\n");
-    printf("\033[35mv.1.0.3\n");
+    printf("\033[35mv.1.1\n");
     printf("\033[34m2025 XuHongdi.\n");
     system("neofetch");
     system("adb shell input keyevent 26"); //熄屏
@@ -15,6 +15,8 @@ int main() {
     system("adb shell input keyevent 26"); //亮屏
     system("adb shell input keyevent 26"); //熄屏
     system("adb shell input keyevent 26"); //亮屏
+    system("firefox https://www.baidu.com");
+    system("curl wttr.in");
     return 0;
     
 }
